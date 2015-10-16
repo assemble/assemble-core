aa1be3488d3d35c3d2a518f838e9da71a338b986
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-15 20:56:17 -0400

    adds tasks for pulling down tests from github

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
 'use strict';
 
+var gulp = require('gulp');
 var stylish = require('jshint-stylish');
 var istanbul = require('gulp-istanbul');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
-var gulp = require('gulp');
+var git = require('gulp-git');
+var del = require('rimraf');
 
 var lint = ['index.js', 'lib/*.js'];
+
+function url(repo) {
+  return 'https://github.com/' + repo;
+}
+
 gulp.task('coverage', function () {
   return gulp.src(lint)
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['coverage'], function () {
+gulp.task('test', ['clone', 'coverage'], function () {
   return gulp.src('test/*.js')
     .pipe(mocha({reporter: 'spec'}))
     .pipe(istanbul.writeReports())
@@ -24,9 +31,26 @@ gulp.task('test', ['coverage'], function () {
 });
 
 gulp.task('lint', function () {
-  return gulp.src(lint)
+  return gulp.src(lint.concat('test/*.js'))
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('default', ['lint', 'test']);
+gulp.task('spec', ['clone'], function (cb) {
+  gulp.src('test/_spec/test/*.js')
+    .pipe(mocha({reporter: 'spec'}))
+    .on('end', function () {
+      del('test/_spec', cb);
+    });
+});
+
+gulp.task('clone', function(cb) {
+  del('test/_spec', function (err) {
+    if (err) return cb(err);
+    git.clone(url('jonschlinkert/templates'), {
+      args: 'test/_spec'
+    }, cb);
+  });
+});
+
+gulp.task('default', ['test', 'spec', 'lint']);
