f197e7188512ac1b930d1f9ceb783042a57f8399
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-20 20:03:17 -0500

    use `base-tasks` plugin, bump deps.
    - version 0.8.0 of composer changes how task names are handled, see the composer docs for more info.
    - templates 0.5.0 uses the latest base-methods, which adds a static `.use` method for pre-registering global plugins.

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
  * module dependencies
  */
 
-var only = require('emitter-only');
 var Templates = require('templates');
-var Composer = require('composer');
+var only = require('emitter-only');
+var tasks = require('base-tasks');
 
 /**
  * Create an `assemble` application. This is the main function exported
@@ -24,52 +24,42 @@ function Assemble(options) {
   if (!(this instanceof Assemble)) {
     return new Assemble(options);
   }
+
   Templates.call(this, options);
-  Composer.call(this, this.options.name || 'assemble');
-  this.init();
+  this.use(tasks(this.options.name || 'assemble'));
+
+  /**
+   * Allow events to be registered only once, so
+   * that we can reinitialize the application but
+   * avoid re-registering the same emitters.
+   */
+
+  this.mixin('only', only.bind(this));
+
+  /**
+   * Load core plugins
+   */
+
+  this.use(require('assemble-fs'));
+  this.use(require('assemble-streams'));
+  this.use(require('assemble-render-file')());
 }
 
 /**
  * `Assemble` prototype methods
  */
 
-Templates.extend(Assemble, {
-  constructor: Assemble,
-
-  /**
-   * Initialize Assemble defaults
-   */
-
-  init: function() {
-
-    /**
-     * Allow events to be registered only once, so
-     * that we can reinitialize the application but
-     * avoid re-registering the same emitters.
-     */
-
-    this.mixin('only', only.bind(this));
-
-    /**
-     * Load core plugins
-     */
-
-    this.use(require('assemble-fs'));
-    this.use(require('assemble-streams'));
-    this.use(require('assemble-render-file')());
-  }
-});
-
-/**
- * Inherit Composer
- */
-
-Templates.inherit(Assemble, Composer);
-Assemble.utils = Templates.utils;
-Assemble._ = Templates._;
+Templates.extend(Assemble);
 
 /**
  * Expose the `Assemble` constructor
  */
 
 module.exports = Assemble;
+
+/**
+ * Expose static properties for unit tests
+ */
+
+Assemble.utils = Templates.utils;
+Assemble._ = Templates._;
