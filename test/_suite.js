649779076a2329879e4d880c8f63805e5c772292
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-26 16:50:47 -0400

    bump deps, update unit tests

--- test/_suite.js
+++ test/_suite.js
@@ -1,17 +1,14 @@
 'use strict';
 
-var suite = require('base-test-suite');
 var runner = require('base-test-runner')();
+var suite = require('base-test-suite');
 
 /**
  * Run the tests in `base-test-suite`
  */
 
-runner.on('file', function(file) {
+runner.on('templates', function(file) {
   require(file.path)(require('..'));
 });
 
-runner.addFiles(suite.test.templates);
-if (suite.test['assemble-core']) {
-  runner.addFiles(suite.test['assemble-core']);
-}
+runner.addFiles('templates', suite.test.templates);
